00ac7f70b6ae3839e3decda4712205ab0b2d963b
S M Ghulam Ghaus Faiyaz <112707106+SMGhulam29091993@users.noreply.github.com>
2026-05-02 23:44:07 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashed = defaultdict(list)
+
+        for cha in strs:
+            count = [0] * 26
+
+            for words in cha:
+                count[ord(words) - ord('a')] += 1
+            
+            hashed[tuple(count)].append(cha)
+        return list(hashed.values())
+
+        # below is O(n * logn) time complexity because of sorting
+        # res = []
+        # for cha in strs:
+        #     hash_key = "".join(sorted(cha))
+        #     if hash_key not in hashed: 
+        #         hashed[hash_key] = []
+        #     hashed[hash_key].append(cha)        
+        # return list(hashed.values())
